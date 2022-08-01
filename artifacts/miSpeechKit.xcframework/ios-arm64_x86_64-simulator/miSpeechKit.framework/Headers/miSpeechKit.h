b7f281c912dc6809721613bc08c74e9fa6b47a24
Jonas Curth <curth@mediainterface.de>
2022-08-01 11:11:44 +0200

    update package name to miSpeechKit

---
+++ artifacts/miSpeechKit.xcframework/ios-arm64_x86_64-simulator/miSpeechKit.framework/Headers/miSpeechKit.h
@@ -0,0 +1,16 @@
+//
+//  iOSSDK.h
+//  iOSSDK
+//
+//  Created by Curth, Jonas | MediaInterface GmbH on 16.11.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for miSpeechKit.
+FOUNDATION_EXPORT double miSpeechKitVersionNumber;
+
+//! Project version string for miSpeechKit.
+FOUNDATION_EXPORT const unsigned char miSpeechKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <miSpeechKit/PublicHeader.h>
